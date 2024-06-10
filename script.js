420fdf78f1bb35284fa0b1f8404652e9ccc47724
ElfoGalatico <edudev.miguel@gmail.com>
2024-06-10 16:22:10 -0300

    atualizações gerais

--- script.js
+++ script.js
@@ -5,23 +5,24 @@ let minuto = agora.getMinutes()
 const corpo = document.querySelector('body')
 const imagem = document.querySelector('img')
 const resultado = document.querySelector('div')
+function carregar() {
+    resultado.innerText = `São ${hora}:${minuto} da `
 
-resultado.innerText = `São ${hora}:${minuto} da `
-
-if(hora < 5){
-    resultado.innerText += ' madrugada'
-    imagem.src = 'imagens/madrugada.jpg'
-    corpo.style.background = '#E8C4E6'
-}else if(hora < 12){
-    resultado.innerText += ' manhã'
-    imagem.src = 'imagens/manha.jpg'
-    corpo.style.background = '#4E6F46'
-}else if(hora < 18){
-    resultado.innerText += ' tarde'
-    imagem.src = 'imagens/tarde.jpg'
-    corpo.style.background = '#BA7975'
-}else{
-    resultado.innerText += ' noite'
-    imagem.src = 'imagens/noite.jpg'
-    corpo.style.background = '#392A45'
+    if (hora < 5) {
+        resultado.innerText += ' madrugada'
+        imagem.src = 'imagens/madrugada.jpg'
+        corpo.style.background = '#E8C4E6'
+    } else if (hora < 12) {
+        resultado.innerText += ' manhã'
+        imagem.src = 'imagens/manha.jpg'
+        corpo.style.background = '#4E6F46'
+    } else if (hora < 18) {
+        resultado.innerText += ' tarde'
+        imagem.src = 'imagens/tarde.jpg'
+        corpo.style.background = '#BA7975'
+    } else {
+        resultado.innerText += ' noite'
+        imagem.src = 'imagens/noite.jpg'
+        corpo.style.background = '#392A45'
+    }
 }
