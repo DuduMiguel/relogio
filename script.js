43d8c6fc8ca18c2fba819cb5fe9b4185e361d65c
ElfoGalatico <edudev.miguel@gmail.com>
2024-06-10 19:12:11 -0300

    adicionei saudação

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ function carregar() {
     resultado.innerText = `São ${hora}:${minuto} da `
 
     if (hora < 5) {
-        resultado.innerText += ' madrugada'
+        resultado.innerText += ' madrugada, boa madrugada!'
         imagem.src = 'imagens/madrugada.jpg'
         corpo.style.background = '#E8C4E6'
     } else if (hora < 12) {
-        resultado.innerText += ' manhã'
+        resultado.innerText += ' manhã, bom dia!'
         imagem.src = 'imagens/manha.jpg'
         corpo.style.background = '#4E6F46'
     } else if (hora < 18) {
-        resultado.innerText += ' tarde'
+        resultado.innerText += ' tarde, boa tarde!'
         imagem.src = 'imagens/tarde.jpg'
         corpo.style.background = '#BA7975'
     } else {
-        resultado.innerText += ' noite'
+        resultado.innerText += ' noite, boa noite!'
         imagem.src = 'imagens/noite.jpg'
         corpo.style.background = '#392A45'
     }
